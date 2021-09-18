da2a8d05722ca8a4cd58b7c71bdbb8e356ed8fc8
Logan Henson <logan@loganhenson.com>
2021-09-18 16:41:11 -0500

    update port handler

--- terminal-pro/src/PortHandlers.elm
+++ terminal-pro/src/PortHandlers.elm
@@ -1,6 +1,7 @@
 module PortHandlers exposing (..)
 
 import Editor.Msg
+import Editor exposing (initialPorts)
 import Json.Encode
 import Ports
 import Terminal.Types
@@ -23,18 +24,6 @@ terminalPorts cwd =
 
 editorPorts : String -> Editor.Msg.Ports
 editorPorts cwd =
-    { requestPaste = Ports.requestPaste
-    , requestRun =
-        \code ->
-            Cmd.none
+    { initialPorts | requestPaste = Ports.requestPaste
     , requestCopy = Ports.requestCopy
-    , requestCompletion =
-        \completionRequest ->
-            Cmd.none
-    , requestChange =
-        \code ->
-            Cmd.none
-    , requestSave =
-        \code ->
-            Cmd.none
     }
